5033e69ecd27d4d670efdd0c58e9e4ac31d9aeb1
gvkrishna1978 <51528947+gvkrishna1978@users.noreply.github.com>
2021-05-09 14:31:56 +0200

    Add files via upload

--- src/main/java/com/spring/openshift/api/calcController.java
+++ src/main/java/com/spring/openshift/api/calcController.java
@@ -3,15 +3,20 @@ package com.spring.openshift.api;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 
 @Controller
 public class calcController {
 	
 	@RequestMapping("calc")	
-	public String calc()
+	public ModelAndView calc()
 	{
-		return "calc.jsp";
+		ModelAndView mv = new ModelAndView();
+		
+		mv.setViewName("calc");
+		
+		return mv;
 	}
 	
 	
--- src/main/java/com/spring/openshift/api/indexController.java
+++ src/main/java/com/spring/openshift/api/indexController.java
@@ -10,7 +10,7 @@ public class indexController {
 	
 	 @RequestMapping("/")
 	public String welcome() {
-		return "Welcome to Calculator <a href='/calc'>Calculator</a>";
+		return "Welcome to Calculator **************   <a href='/calc'>Calculator</a>";
 	}
 
 }
